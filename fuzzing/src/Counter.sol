b772e5062aa38dcd65c37aa0568e74af8298a993
joesepherus <joesepherus@gmail.com>
2024-11-26 13:34:49 +0800

    Formatting.

--- fuzzing/src/Counter.sol
+++ fuzzing/src/Counter.sol
@@ -1,16 +1,15 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-
 contract Counter {
-    uint256 public count;
+  uint256 public count;
 
-    function increment(uint256 value) public {
-        count += value;
-    }
+  function increment(uint256 value) public {
+    count += value;
+  }
 
-    function decrement(uint256 value) public {
-        require(count >= value, "Underflow error");
-        count -= value;
-    }
+  function decrement(uint256 value) public {
+    require(count >= value, "Underflow error");
+    count -= value;
+  }
 }
--- fuzzing/test/Counter.t.sol
+++ fuzzing/test/Counter.t.sol
@@ -5,25 +5,25 @@ import {Test, console} from "forge-std/Test.sol";
 import {Counter} from "../src/Counter.sol";
 
 contract CounterTest is Test {
-    Counter counter;
+  Counter counter;
 
-    function setUp() public {
-        counter = new Counter();
-    }
+  function setUp() public {
+    counter = new Counter();
+  }
 
-    function testFuzzIncrement(uint256 value) public {
-         emit log_named_uint("Generated value", value);
-        uint256 initialCount = counter.count();
-        counter.increment(value);
-        assertEq(counter.count(), initialCount + value, "Increment failed");
-    }
+  function testFuzzIncrement(uint256 value) public {
+    emit log_named_uint("Generated value", value);
+    uint256 initialCount = counter.count();
+    counter.increment(value);
+    assertEq(counter.count(), initialCount + value, "Increment failed");
+  }
 
-    function testFuzzDecrement(uint256 value) public {
-        // Ensure the initial state is large enough for decrement
-        counter.increment(value);
-        uint256 initialCount = counter.count();
+  function testFuzzDecrement(uint256 value) public {
+    // Ensure the initial state is large enough for decrement
+    counter.increment(value);
+    uint256 initialCount = counter.count();
 
-        counter.decrement(value);
-        assertEq(counter.count(), initialCount - value, "Decrement failed");
-    }
+    counter.decrement(value);
+    assertEq(counter.count(), initialCount - value, "Decrement failed");
+  }
 }
